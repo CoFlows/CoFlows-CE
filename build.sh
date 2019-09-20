a13ef53dddd42676ec245a6b829f53d24c62d704
Arturo Rodriguez <arturo.rodriguez@quantapp.net>
2019-09-20 19:19:24 +0100

    add quant libraries

--- build.sh
+++ build.sh
@@ -5,13 +5,13 @@ ng build --prod --aot
 
 cd ../
 
-dotnet clean CoFlows.CE.sln
-dotnet publish -c Release -f netcoreapp2.2 -o obj/Docker/publish CoFlows.CE.sln
+dotnet clean CoFlows.Quant.sln
+dotnet publish -c Release -f netcoreapp2.2 -o obj/Docker/publish CoFlows.Quant.sln
 
 make
 
 cd QuantApp.Server
 
-docker build -t coflows/ce .
-docker tag coflows/ce quantapp/coflows:ce
-docker push quantapp/coflows:ce
+docker build -t coflows/quant .
+# docker tag coflows/quant quantapp/coflows:quant
+# docker push quantapp/coflows:quant
