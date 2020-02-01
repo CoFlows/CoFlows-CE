4c03485ab5f7b0edb63b1e409775794092065901
Arturo Rodriguez <arturo.rodriguez@quantapp.net>
2020-02-01 23:45:24 +1100

    update

--- QuantApp.Server/Quant.Program.cs
+++ QuantApp.Server/Quant.Program.cs
@@ -27,6 +27,13 @@ using Newtonsoft.Json.Linq;
 
 using AQI.AQILabs.Kernel;
 
+//Azure Dependencies
+using Microsoft.Azure.Management.ContainerInstance.Fluent;
+using Microsoft.Azure.Management.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Authentication;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
+
 namespace QuantApp.Server.Quant
 {
     public class Program
@@ -60,7 +67,15 @@ namespace QuantApp.Server.Quant
                 typeof(AQI.AQILabs.SDK.Strategies.PortfolioStrategy)
                 });
 
-            JObject config = (JObject)JToken.ReadFrom(new JsonTextReader(File.OpenText(@"mnt/quantapp_config.json")));
+            var config_env = Environment.GetEnvironmentVariable("coflows_config");
+            var config_file = Environment.GetEnvironmentVariable("config_file");
+
+            if(string.IsNullOrEmpty(config_file))
+                config_file = "quantapp_config.json";
+
+            JObject config = string.IsNullOrEmpty(config_env) ? (JObject)JToken.ReadFrom(new JsonTextReader(File.OpenText(@"mnt/" + config_file))) : (JObject)JToken.Parse(config_env);
+
+            // JObject config = (JObject)JToken.ReadFrom(new JsonTextReader(File.OpenText(@"mnt/quantapp_config.json")));
             workspace_name = config["Workspace"].ToString();
             hostName = config["Server"]["Host"].ToString();
             var secretKey = config["Server"]["SecretKey"].ToString();
@@ -268,9 +283,16 @@ namespace QuantApp.Server.Quant
 
                 Console.WriteLine("Local deployment");
 
-                var pkg = Code.ProcessPackageFile(workspace_name);
-                Code.ProcessPackageJSON(pkg);
-                SetDefaultWorkSpaces(new string[]{ pkg.ID });
+                if(string.IsNullOrEmpty(config_env))
+                {
+                    var pkg = Code.ProcessPackageFile(workspace_name);
+                    Code.ProcessPackageJSON(pkg);
+                    SetDefaultWorkSpaces(new string[]{ pkg.ID });
+                    Console.WriteLine(pkg.Name + " started");
+                }
+                else
+                    Console.WriteLine("Empty server...");
+
 
                 /// QuantSpecific START
                 Instrument.TimeSeriesLoadFromDatabaseIntraday = config["Quant"]["Intraday"].ToString().ToLower() == "true";
@@ -311,6 +333,277 @@ namespace QuantApp.Server.Quant
                 });
                 Console.CancelKeyPress += new ConsoleCancelEventHandler(OnExit);
                 _closing.WaitOne();
+            }
+            else if(args != null && args.Length > 1 && args[0] == "local" && args[1] == "build")
+            {
+                PythonEngine.BeginAllowThreads();
+
+                if(config["Database"].Children().Count() > 0)
+                {
+                    var kernelString = config["Database"]["Kernel"].ToString();
+                    var strategyString = config["Database"]["Strategies"].ToString();
+                    var quantappString = config["Database"]["QuantApp"].ToString();
+                    Databases(kernelString, strategyString, quantappString);
+                }
+                else
+                {
+                    var connectionString = config["Database"].ToString();
+                    Databases(connectionString);
+                }
+                Console.WriteLine("DB Connected");
+
+                Console.WriteLine("Local build");
+
+                var pkg = Code.ProcessPackageFile(Code.UpdatePackageFile(workspace_name));
+                var res = Code.BuildRegisterPackage(pkg);
+
+                if(string.IsNullOrEmpty(res))
+                    Console.WriteLine("Success!!!");
+                else
+                    Console.WriteLine(res);
+            }
+            else if(args != null && args.Length > 2 && args[0] == "local" && args[1] == "query")
+            {
+                PythonEngine.BeginAllowThreads();
+
+                if(config["Database"].Children().Count() > 0)
+                {
+                    var kernelString = config["Database"]["Kernel"].ToString();
+                    var strategyString = config["Database"]["Strategies"].ToString();
+                    var quantappString = config["Database"]["QuantApp"].ToString();
+                    Databases(kernelString, strategyString, quantappString);
+                }
+                else
+                {
+                    var connectionString = config["Database"].ToString();
+                    Databases(connectionString);
+                }
+                Console.WriteLine("Local Query " + DateTime.Now);
+                Console.WriteLine("DB Connected");
+
+                Console.WriteLine("CoFlows Local query... ");
+
+                var queryID = args[2];
+                var funcName = args.Length > 3 ? args[3] : null;
+                var parameters = args.Length > 4 ? args.Skip(4).ToArray() : null;
+
+                Console.WriteLine("QueryID: " + queryID);
+                Console.WriteLine("FuncName: " + funcName);
+                Console.WriteLine("Parameters: " + parameters);
+
+
+                var pkg = Code.ProcessPackageFile(Code.UpdatePackageFile(workspace_name));
+                Code.ProcessPackageJSON(pkg);
+                
+
+                if(parameters != null)
+                    for(int i = 0; i < parameters.Length; i++)
+                        Console.WriteLine("Parameter[" + i + "]: " + parameters[i]);
+
+                
+                var (code_name, code) = pkg.Queries.Where(entry => entry.ID == queryID).Select(entry => (entry.Name as string, entry.Content as string)).FirstOrDefault();
+                var t0 = DateTime.Now;
+                Console.WriteLine("Started: " + t0);
+
+                // var wb = wb_res.FirstOrDefault() as CodeData;
+                var codes = new List<Tuple<string,string>>();
+                codes.Add(new Tuple<string, string>(code_name, code));
+
+                var result = QuantApp.Engine.Utils.ExecuteCodeFunction(false, codes, funcName, parameters);
+                //var result = Connection.Client.Execute(code, code_name, pkg.ID, queryID, funcName, parameters);
+                var t1 = DateTime.Now;
+                Console.WriteLine("Ended: " + t1 + " taking " + (t1 - t0));
+                
+                Console.WriteLine("Result: ");
+                Console.WriteLine(result);
+            }
+
+            //Azure Container Instance
+            else if(args != null && args.Length > 1 && args[0] == "azure" && args[1] == "deploy")
+            {
+                PythonEngine.BeginAllowThreads();
+
+                Console.WriteLine();
+                Console.WriteLine("Azure Container Instance start...");
+                var t0 = DateTime.Now;
+                Console.WriteLine("Started: " + t0);
+
+
+                var pkg = Code.ProcessPackageFile(Code.UpdatePackageFile(workspace_name));
+                var res = Code.BuildRegisterPackage(pkg);
+
+                if(string.IsNullOrEmpty(res))
+                    Console.WriteLine("Build Success!!!");
+                else
+                    Console.WriteLine(res);
+
+                AzureCredentials credentials = SdkContext.AzureCredentialsFactory.FromFile(config["AzureContainerInstance"]["AuthFile"].ToString());
+
+                var azure = Azure
+                    .Configure()
+                    .WithLogLevel(HttpLoggingDelegatingHandler.Level.Basic)
+                    .Authenticate(credentials)
+                    .WithDefaultSubscription();
+
+                string rgName = pkg.ID.ToLower() + "-rg";
+                string aciName = pkg.Name.ToLower();
+                string containerImageName = "coflows/quant";
+                
+                Console.WriteLine("aciName: " + aciName);
+                Console.WriteLine("rgName: " + rgName);
+
+                try
+                {
+                    Console.WriteLine("Cleaning Resource Group: " + rgName);
+                    azure.ResourceGroups.BeginDeleteByName(rgName);
+                    
+
+                    IResourceGroup resGroup = azure.ResourceGroups.GetByName(rgName);
+                    while(resGroup != null)
+                    {
+                        resGroup = azure.ResourceGroups.GetByName(rgName);
+
+                        Console.Write(".");
+
+                        SdkContext.DelayProvider.Delay(1000);
+                    }
+                    Console.WriteLine();
+
+                    Console.WriteLine("Cleaned Resource Group: " + rgName);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Did not create any resources in Azure. No clean up is necessary");
+                }
+                
+                Region region = Region.Create(config["AzureContainerInstance"]["Region"].ToString());
+                Console.WriteLine("region: " + region);
+                
+                Task.Run(() =>
+                    azure.ContainerGroups.Define(aciName)
+                    .WithRegion(region)
+                    .WithNewResourceGroup(rgName)
+                    .WithLinux()
+                    .WithPublicImageRegistryOnly()
+                    // .WithNewAzureFileShareVolume(volumeMountName, shareName)
+                    .WithoutVolume()
+                    .DefineContainerInstance(aciName)
+                        .WithImage(containerImageName)
+                        .WithExternalTcpPort(sslFlag ? 443 : 80)
+                        // .WithVolumeMountSetting(volumeMountName, "/aci/logs/")
+                        .WithCpuCoreCount(Int32.Parse(config["AzureContainerInstance"]["Cores"].ToString()))
+                        .WithMemorySizeInGB(Int32.Parse(config["AzureContainerInstance"]["Mem"].ToString()))
+                        // .WithGpuResource(0, GpuSku.V100)
+                        .WithEnvironmentVariables(new Dictionary<string,string>(){ 
+                            {"coflows_config", File.ReadAllText(@"mnt/" + config_file)}, 
+                            })
+                        .WithStartingCommandLine("dotnet", "QuantApp.Server.quant.lnx.dll", "server")
+                        .Attach()
+                    .WithDnsPrefix(config["AzureContainerInstance"]["Dns"].ToString()) 
+                    .CreateAsync()
+                );
+
+                // Poll for the container group
+                IContainerGroup containerGroup = null;
+                while(containerGroup == null)
+                {
+                    containerGroup = azure.ContainerGroups.GetByResourceGroup(rgName, aciName);
+
+                    Console.Write(".");
+
+                    SdkContext.DelayProvider.Delay(1000);
+                }
+
+                var lastContainerGroupState = containerGroup.Refresh().State;
+
+                Console.WriteLine();
+                Console.WriteLine($"Container group state: {containerGroup.Refresh().State}");
+                // Poll until the container group is running
+                while(containerGroup.State != "Running")
+                {
+                    var containerGroupState = containerGroup.Refresh().State;
+                    if(containerGroupState != lastContainerGroupState)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine(containerGroupState);
+                        lastContainerGroupState = containerGroupState;
+                    }
+                    Console.Write(".");
+                    
+                    System.Threading.Thread.Sleep(1000);
+                }
+                Console.WriteLine();
+                Console.WriteLine("Container instance IP address: " + containerGroup.IPAddress);
+                Console.WriteLine("Container instance Ports: " + string.Join(",", containerGroup.ExternalTcpPorts));
+
+                string serverUrl = config["AzureContainerInstance"]["Dns"].ToString() + "." + config["AzureContainerInstance"]["Region"].ToString().ToLower() + ".azurecontainer.io";
+                Console.WriteLine("Container instance DNS Prefix: " + serverUrl);
+                SdkContext.DelayProvider.Delay(10000);
+
+                Connection.Client.Init(serverUrl, sslFlag);
+
+                if(!Connection.Client.Login(config["Server"]["SecretKey"].ToString()))
+                    throw new Exception("CoFlows Not connected!");
+
+                Connection.Client.Connect();
+                Console.Write("Container connected! ");
+
+                QuantApp.Kernel.M.Factory = new MFactory();
+
+                Console.Write("Starting azure deployment... ");
+
+                Code.UpdatePackageFile(workspace_name);
+                var resDeploy = Connection.Client.PublishPackage(workspace_name);
+                var t1 = DateTime.Now;
+                Console.WriteLine("Ended: " + t1 + " taking " + (t1 - t0));
+                Console.Write("Result: " + resDeploy);
+            }
+            else if(args != null && args.Length > 1 && args[0] == "azure" && args[1] == "remove")
+            {
+                PythonEngine.BeginAllowThreads();
+                Console.WriteLine();
+                Console.WriteLine("Azure Container Instance remove start");
+
+                var pkg = Code.ProcessPackageFile(Code.UpdatePackageFile(workspace_name));
+                var res = Code.BuildRegisterPackage(pkg);
+
+                if(!string.IsNullOrEmpty(res))
+                    Console.WriteLine(res);
+
+                AzureCredentials credentials = SdkContext.AzureCredentialsFactory.FromFile(config["AzureContainerInstance"]["AuthFile"].ToString());
+
+                var azure = Azure
+                    .Configure()
+                    .WithLogLevel(HttpLoggingDelegatingHandler.Level.Basic)
+                    .Authenticate(credentials)
+                    .WithDefaultSubscription();
+
+                string rgName = pkg.ID.ToLower() + "-rg";
+                string aciName = pkg.Name.ToLower();
+                
+                try
+                {
+                    Console.WriteLine("Deleting Resource Group: " + rgName);
+                    azure.ResourceGroups.BeginDeleteByName(rgName);
+                    
+
+                    IResourceGroup resGroup = azure.ResourceGroups.GetByName(rgName);
+                    while(resGroup != null)
+                    {
+                        resGroup = azure.ResourceGroups.GetByName(rgName);
+
+                        Console.Write(".");
+
+                        SdkContext.DelayProvider.Delay(1000);
+                    }
+                    Console.WriteLine();
+
+                    Console.WriteLine("Deleted Resource Group: " + rgName);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Did not create any resources in Azure. No clean up is necessary");
+                }
             }
             else
                 Console.WriteLine("Wrong argument");
