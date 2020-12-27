52b040b1b8c75a2bd5d5ae14547968ef7f571676
CoFlows <arturo@quant.app>
2020-12-27 18:40:28 +1100

    Update app.component.ts

--- CoFlows.Client/CE/src/app/workflows/app/app.component.ts
+++ CoFlows.Client/CE/src/app/workflows/app/app.component.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CoFlowsComponent } from '../../coflows/core/coflows.component';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
-// import { Location } from "@angular/common";
+import { Location } from "@angular/common";
 
 import { DomSanitizer } from '@angular/platform-browser';
 
@@ -14,24 +14,19 @@ export class AppComponent {
 
     url = {}
 
-    // constructor(private readonly location: Location, private activatedRoute: ActivatedRoute, private router: Router, public coflows: CoFlowsComponent, private sanitization:DomSanitizer){
-    constructor(private activatedRoute: ActivatedRoute, public coflows: CoFlowsComponent, private sanitization:DomSanitizer){
+    constructor(private readonly location: Location, private activatedRoute: ActivatedRoute, private router: Router, public coflows: CoFlowsComponent, private sanitization:DomSanitizer){
 
-    var count = 0
-
-    this.activatedRoute.url.subscribe(params => {
-        console.log('LOAD APP', count)
-        if(count == 0){
-            console.log(params)
-            let cleanUrl = params.map(x => x.path).join('/')
-            // let cleanUrl = params.map(x => decodeURIComponent(x.path)).join('/')
-            let fullUrl = this.coflows.coflows_server + cleanUrl
-            this.url = this.sanitization.bypassSecurityTrustResourceUrl(fullUrl)
-            count++
-            // this.location.go(cleanUrl)
-        }
-        
-    });
-
+        var n=0
+        this.activatedRoute.url.subscribe(params => {
+            if(n == 0)
+            {
+                let cleanUrl = params.map(x => decodeURIComponent(x.path)).join('/')
+                let fullUrl = this.coflows.coflows_server + cleanUrl
+                this.url = this.sanitization.bypassSecurityTrustResourceUrl(fullUrl)
+                n++
+                this.location.go('/workflows/app/' + cleanUrl)
+            }
+            
+        })
     }
 }
